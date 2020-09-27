c265627622e2a05837fd12a9e0bd9803a4896f80
Alexander Murzanaev <amoneron@gmail.com>
2020-09-27 16:14:43 +0300

    Fetch test

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const { json } = require('micro');
+const connect = require('request');
+var options = {
+    'method': 'GET',
+    'url': 'https://api.treep.ai/bot/activities/random?count=1',
+    'headers': {
+        'x-api-key': 'treep_appcraft'
+    }
+};
 
 module.exports = async (req, res) => {
     const { request, session, version } = await json(req);
-    res.end(JSON.stringify(
-        {
-            version,
-            session,
-            response: {
-                text: request.original_utterance || 'Hello!',
-                end_session: false,
-            },
-        }
-    ));
+
+    connect(options, function (error, response) {
+        if (error) throw new Error(error);
+        
+        res.end(JSON.stringify(
+            {
+                version,
+                session,
+                response: {
+                    text: 'Response',
+                    end_session: false,
+                },
+            }
+        ));
+    
+    });
+
+    // res.end(JSON.stringify(
+    //     {
+    //         version,
+    //         session,
+    //         response: {
+    //             text: request.original_utterance || 'Hello!',
+    //             end_session: false,
+    //         },
+    //     }
+    // ));
 };
