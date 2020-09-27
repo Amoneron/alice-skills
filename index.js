88f4b5054f4498b5af27375efb34bc1ee34839a4
Alexander Murzanaev <amoneron@gmail.com>
2020-09-27 16:45:44 +0300

    Complete answers

--- index.js
+++ index.js
@@ -8,34 +8,39 @@ var options = {
     },
     'json': true
 };
+var intros = ['Можно', 'А еще можно было бы', 'Например, можно', 'Как насчет'];
 
 module.exports = async (req, res) => {
     const { request, session, version } = await json(req);
 
-    connect(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        
-        res.end(JSON.stringify(
-            {
-                version,
-                session,
-                response: {
-                    text: body['name'] || 'К сожалению, сервер недоступен',
-                    end_session: false,
-                },
-            }
-        ));
-    
-    });
+    function resp(answer, end) {
+        res.end(JSON.stringify({
+            version,
+            session,
+            response: {
+                text: answer,
+                end_session: end,
+            },
+        }));
+    }
 
-    // res.end(JSON.stringify(
-    //     {
-    //         version,
-    //         session,
-    //         response: {
-    //             text: request.original_utterance || 'Hello!',
-    //             end_session: false,
-    //         },
-    //     }
-    // ));
+    if (request.original_utterance == nil) {
+        resp('Привет! Могу подсказать, чем можно заняться в свободное время. По любой команде придумаю следующую идею. По стоп-слову Хватит прекращу предлагать варианты. Итак, поехали?', false);
+        return;
+    }
+
+    if (request.original_utterance.toLowerCase() == 'хватит') {
+        resp('Без проблем. Надеюсь, мне удалось подсказать что-то прекрасное. Обращайтесь за новыми идеями. Прекрасного дня!', true);
+        return;
+    }
+
+    connect(options, function (error, response, body) {
+        if (error || body['name'] == nil) { 
+            resp('К сожалению, сервер недоступен. А без него я ничего не могу сделать. Попробуйте спросить меня пожалуйста позже о том, что хотелось.', true);
+        } else {
+            var intro = intros[Math.floor(Math.random() * intros.length)];
+            resp(intro + ' ' + body['name'].toLowerCase() + '. ' + body['brief'], false);
+        }    
+    });
+    
 };
